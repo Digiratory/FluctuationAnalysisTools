cc6f731f5b7b8b2de00987682b625cc33d8d3703
Asya Lyanova <64890333+pipipyau@users.noreply.github.com>
2025-09-26 13:14:54 +0300

    feat: kalman settings

--- research/kalman_filter/kalman_experiments.py
+++ research/kalman_filter/kalman_experiments.py
@@ -1,5 +1,6 @@
 import multiprocessing
 import warnings
+
 import numpy as np
 import pandas as pd
 from tqdm import tqdm
@@ -9,12 +10,16 @@ from StatTools.experimental.augmentation.perturbations import (
     add_noise,
     add_poisson_gaps,
 )
-from StatTools.experimental.analysis.tools import get_extra_h_dfa
-from StatTools.experimental.synthesis.tools import adjust_hurst_to_range, reverse_hurst_adjustment
+from StatTools.experimental.synthesis.tools import (
+    adjust_hurst_to_range,
+    reverse_hurst_adjustment,
+)
 from StatTools.filters.kalman_filter import FractalKalmanFilter
 from StatTools.generators.kasdin_generator import create_kasdin_generator
+from StatTools.utils import io
 
 warnings.filterwarnings("ignore")
+
 
 def process_single_iter(args):
     """Обрабатывает одну итерацию с заданным H, s (порядок ФФ) и возвращает результаты"""
@@ -29,7 +34,7 @@ def process_single_iter(args):
             gaps_signals["gaps_eq_signal"] = add_poisson_gaps(signal, 0.1, s)
             gaps_signals["gaps_long_signal"] = add_poisson_gaps(signal, 0.1, s * 2)
             for name, gap_signal in gaps_signals.items():
-                restored_signal = apply_kalman_filter(signal, gap_signal, h, r)
+                restored_signal = apply_kalman_filter(gap_signal, h, r)
                 metrics = get_metrics(signal, restored_signal)
                 if name == "gaps_short_signal":
                     gaps_len = s / 2
@@ -52,6 +57,7 @@ def process_single_iter(args):
                 )
     return results_local
 
+
 def process_snr_h_iter(args):
     """Обрабатывает одну итерацию с заданным H, s (порядок ФФ), SNR n_times и возвращает результаты"""
     h, s, r_list, trj_len, snr, n_times = args
@@ -70,19 +76,19 @@ def process_snr_h_iter(args):
             se = np.nanstd(signal[0 : len(estimated_signal)] - estimated_signal)
             h_est = get_extra_h_dfa(estimated_signal)
             results_local.append(
-                    {
-                        "H_target":h,
-                        "H_signal":h_s,
-                        "H_estimated": h_est,
-                        "signal_len":len(signal),
-                        "s":s,
-                        "r":r,
-                        "SNR":snr,
-                        "SE":se,
-                    })
+                {
+                    "H_target": h,
+                    "H_signal": h_s,
+                    "H_estimated": h_est,
+                    "signal_len": len(signal),
+                    "s": s,
+                    "r": r,
+                    "SNR": snr,
+                    "SE": se,
+                }
+            )
     return results_local
-    
-    
+
 
 def get_r_list() -> list:
     return [2, 4, 8]
@@ -98,13 +104,38 @@ def get_signal(h: float, length: int, s: int, normalize=False) -> np.array:
         return signal
 
 
-def apply_kalman_filter(
-    orig_signal: np.array, signal, model_h: np.array, r: int, noise=None
+def apply_kalman_filter(signal, model_h: np.array, r: int, noise=None) -> np.array:
+    f = FractalKalmanFilter(dim_x=r, dim_z=1)
+    if noise is None:
+        noise = np.zeros(len(signal))
+    f_matrix = f.get_filter_matrix(r, model_h, len(signal))
+    r_matrix = np.std(noise) ** 2
+    h_matrix = f.H
+    h_matrix[0][0] = 1.0
+    f.set_matrices(h_matrix, r_matrix, f_matrix)
+
+    estimated_signal = np.zeros(len(signal))
+    for k in range(1, len(signal)):
+        f.predict()
+        if not np.isnan(signal[k]):
+            f.update(signal[k])
+        estimated_signal[k] = f.x[0].item()
+    return estimated_signal
+
+
+def apply_kalman_filter_cached(
+    signal, model_h: np.array, r: int, cache_folder: str, noise=None
 ) -> np.array:
     f = FractalKalmanFilter(dim_x=r, dim_z=1)
     if noise is None:
-        noise = np.zeros(len(orig_signal))
-    f.set_parameters(model_h, np.std(noise) ** 2, kasdin_lenght=len(signal), order=r)
+        noise = np.zeros(len(signal))
+    file_path = cache_folder / f"{model_h}_{r}.npy"
+    f_matrix = io.load_matrix(file_path)
+    r_matrix = np.std(noise) ** 2
+    h_matrix = f.H
+    h_matrix[0][0] = 1.0
+    f.set_matrices(h_matrix, r_matrix, f_matrix)
+
     estimated_signal = np.zeros(len(signal))
     for k in range(1, len(signal)):
         f.predict()
@@ -210,7 +241,7 @@ if __name__ == "__main__":
     for h in H_LIST:
         args_list.append((h, s, R_LIST, TRJ_LEN, snr, n_times))
     print(f"Got {len(args_list) * len(R_LIST)} combinations for {n_times} times.")
-    
+
     print("Run pool")
     with multiprocessing.Pool() as pool:
         results = list(
@@ -220,7 +251,7 @@ if __name__ == "__main__":
                 desc="Progress",
             )
         )
-    
+
     print("Prepare results")
     for res in results:
         for row in res:
@@ -237,4 +268,3 @@ if __name__ == "__main__":
     file_name = "kalman.csv"
     metrics_df.to_csv(file_name, index=False)
     print(f"Metrics saved to {file_name}")
-    
