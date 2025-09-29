1169428b35f5626c5ff0b71abf145d87e1ad814b
Asya Lyanova <64890333+pipipyau@users.noreply.github.com>
2025-09-29 12:23:10 +0300

    format: format

--- StatTools/utils/io.py
+++ StatTools/utils/io.py
@@ -18,13 +18,13 @@ def load_np_matrix(file_path: str) -> np.ndarray:
 def save_sympy_matrix(matrix: Matrix, file_path: Path):
     """Save a matrix to a file (path will be created if not exists)."""
     file_path.parent.mkdir(parents=True, exist_ok=True)
-    with open(file_path, 'w', encoding='utf-8') as f:
+    with open(file_path, "w", encoding="utf-8") as f:
         f.write(str(matrix.tolist()))
 
 
 def load_sympy_matrix(file_path: str) -> Matrix:
     """Load a matrix from a file."""
-    with open(file_path, 'r', encoding='utf-8') as f:
+    with open(file_path, "r", encoding="utf-8") as f:
         content = f.read()
     matrix_list = sympify(content)
     return Matrix(matrix_list)
