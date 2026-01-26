c966b56ec7264ef32fcf1d953a16f5f5dea65fa2
Asya Lyanova <64890333+pipipyau@users.noreply.github.com>
2026-01-26 15:45:26 +0300

    test: add io test

---
+++ tests/test_io_utils.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pytest
+from sympy import Matrix
+
+from StatTools.utils import io
+
+
+def test_save_load_np_matrix(tmp_path):
+    """
+    Test saving and loading numpy matrices.
+    """
+    mat = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    file_path = tmp_path / "test.npy"
+    io.save_np_matrix(mat, file_path)
+    mat_est = io.load_np_matrix(file_path)
+    assert np.allclose(mat, mat_est)
+
+
+def test_save_load_sympy_matrix(tmp_path):
+    """
+    Test saving and loading sympy matrices.
+    """
+    mat = Matrix([[1, -1], [3, 4], [0, 2]])
+    file_path = tmp_path / "test.txt"
+    io.save_sympy_matrix(mat, file_path)
+    mat_est = io.load_sympy_matrix(file_path)
+    assert mat.equals(mat_est)
