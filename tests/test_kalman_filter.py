34d22fedfd8637c0303c71cbe7c6f579daf2a14e
Asya Lyanova <64890333+pipipyau@users.noreply.github.com>
2026-01-30 16:03:53 +0300

    test: KF set_parameters tests

--- tests/test_kalman_filter.py
+++ tests/test_kalman_filter.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest
 
+from StatTools.filters.kalman_filter import FractalKalmanFilter, KalmanParams
 from StatTools.filters.symbolic_kalman import (
     get_sympy_filter_matrix,
     refine_filter_matrix,
@@ -8,7 +9,7 @@ from StatTools.filters.symbolic_kalman import (
 from StatTools.generators.kasdin_generator import KasdinGenerator
 
 
-def test_filter_auto_configure():
+def test_refine_filter_matrix():
     """Test sympy matrix calculation for 2nd, 3rd order filters."""
     h = 0.8
     generator = KasdinGenerator(h, length=2 * 14)
@@ -16,7 +17,6 @@ def test_filter_auto_configure():
     for order in range(2, 4):
         number_matrix = refine_filter_matrix(get_sympy_filter_matrix(order), order, A)
         np_number_matrix = np.array(number_matrix)
-        print(np.array([[-A[1] - A[2], A[2]], [(-1 - A[1] - A[2]), A[2]]]))
         if order == 2:
             assert np.array_equal(
                 np_number_matrix,
@@ -33,3 +33,32 @@ def test_filter_auto_configure():
                     ]
                 ),
             )
+
+
+def test_set_parameters_auto_F():
+    """Test that set parameters with auto F works correctly."""
+    kf = FractalKalmanFilter(dim_x=2, dim_z=1)
+
+    params = KalmanParams(
+        model_h=0.5,
+        noise_var=0.01,
+        kasdin_length=10,
+    )
+    kf.set_parameters(params)
+    assert kf.F.shape == (2, 2)
+
+
+def test_set_parameters_invalid_F_shape():
+    """Test that wrong F shape raises an error."""
+    kf = FractalKalmanFilter(dim_x=2, dim_z=1)
+    wrong_F = np.eye(3)
+
+    params = KalmanParams(
+        model_h=0.5,
+        noise_var=0.01,
+        kasdin_length=10,
+        F=wrong_F,
+    )
+
+    with pytest.raises(ValueError):
+        kf.set_parameters(params)
